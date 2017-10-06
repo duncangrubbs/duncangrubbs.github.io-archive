a5423062eea6e8880ecf2d7fb5f732966a4215f9
Duncan Grubbs <duncan.grubbs@gmail.com>
2017-10-06 15:30:12 -0400

    Add website.

---
+++ README.md
@@ -0,0 +1,14 @@
+# Duncan Grubbs' Personal Website
+
+## Testing the code and starting a server
+- Open a terminal
+- Navigate to the repository
+- Run `python -m SimpleHTTPServer 9000`
+- Go to a browser
+- Go to `localhost:9000`
+
+## Contributing
+- Fork this repo
+- Make your changes
+- Submit a PR!
+
---
+++ text_to_amp.py
@@ -0,0 +1,28 @@
+import os
+
+articles = os.listdir('./articles/')
+# articles.remove('.DS_Store')
+
+amp_head = open('./amp-boiler/head.txt', 'r')
+amp_foot = open('./amp-boiler/foot.txt', 'r')
+head = amp_head.read()
+foot = amp_foot.read()
+
+for a in articles:
+  path = './articles/' + a
+  article = open(path, 'r')
+  article_string = article.read()
+  article.close();
+  full_html = head + article_string + foot
+
+  a = a.replace('txt', 'html')
+  name = './amp-pages/' + a
+  full = open(name, 'w');
+  full.write(full_html);
+  full.close();
+
+amp_head.close();
+amp_foot.close();
+
+
+
